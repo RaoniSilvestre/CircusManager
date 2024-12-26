a62289ff18d20dca6429e1cdbc5da32e22f7ce4d
Donklii <143131269+Donklii@users.noreply.github.com>
2024-12-26 18:53:31 -0300

    Update Circus.java

--- src/main/java/br/ufrn/imd/circusmanager/Model/Circus/Circus.java
+++ src/main/java/br/ufrn/imd/circusmanager/Model/Circus/Circus.java
@@ -1,13 +1,13 @@
 package br.ufrn.imd.circusmanager.Model.Circus;
 
-//import br.ufrn.imd.circusmanager.Dao.*;
+import br.ufrn.imd.circusmanager.Dao.*;
 
 public class Circus {
-    //private FuncionarioDAO funcionarios;
-    //private AnimalDAO animais;
+    private FuncionarioDAO funcionarios;
+    private AnimalDAO animais;
 
-    //public Circus(float saldo) {
-    //    animais = new AnimalDAO();
-    //    funcionarios = new FuncionarioDAO();
-    //}
+    public Circus(float saldo) {
+        animais = new AnimalDAO();
+        funcionarios = new FuncionarioDAO();
+    }
 }
